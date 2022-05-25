9797d703f4be8c101438fa6a2e71d0719082136c
Sun <59905230+ArtidSun@users.noreply.github.com>
2022-05-25 11:31:16 +0700

    edit POST report

--- app/model/entity/report.go
+++ app/model/entity/report.go
@@ -16,7 +16,6 @@ type ReportInsert struct {
 	CategoriesReport string `json:"categoriesReport" gorm:"column:categoriesReport"`
 	ReportDes        string `json:"reportDes" gorm:"column:reportDes"`
 	Status           string `json:"status" gorm:"column:status"`
-	SuccessDate      string `json:"successDate" gorm:"column:successDate"`
 	ReportDate       string `json:"reportDate" gorm:"column:reportDate"`
 	CreatedBy        int    `json:"createdBy" gorm:"column:createdBy"`
 }
