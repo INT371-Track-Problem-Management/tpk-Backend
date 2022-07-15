1f5586f4eae14755d785c1d0154f82e313cd1ccf
Sun <59905230+ArtidSun@users.noreply.github.com>
2022-07-15 17:19:49 +0700

    edit config

--- app/server/server.go
+++ app/server/server.go
@@ -114,6 +114,7 @@ func (h *FuncHandler) Login(ctx echo.Context) error {
 func (h *FuncHandler) Initialize() {
 	db := config.LoadDB()
 	dns := fmt.Sprintf(`%v:%v@tcp(%v:%v)/%v?charset=utf8&parseTime=True&loc=Local`, db.Username, db.Password, db.Host, db.Port, db.Database)
+	log.Println(dns)
 	conn, err := gorm.Open(mysql.Open(dns), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
