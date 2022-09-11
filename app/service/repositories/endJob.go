fe3b635d734e04c0c42a62aea5501daf209134c1
Sun <sun_vijitpanmai@hotmail.com>
2022-09-11 16:25:27 +0700

    fix bug end job

--- app/service/repositories/endJob.go
+++ app/service/repositories/endJob.go
@@ -11,24 +11,36 @@ import (
 func EndJobReport(ctx echo.Context, conn *gorm.DB, req entity.EndJobReport) error {
 	var err error
 	stmt := conn.Begin()
-
-	err = stmt.Table("reports").Where("reportId = ?", req.ReportId).Update("status = ?", "S7").Error
+	sql1 := fmt.Sprintf(
+		`
+		UPDATE reports
+		SET status = 'S7'
+		WHERE reportId = %v
+		`, req.ReportId)
+	err = stmt.Exec(sql1).Error
 	if err != nil {
 		return err
 	}
 
-	sql := fmt.Sprintf(
+	sql2 := fmt.Sprintf(
 		`
 	INSERT INTO reviewReports (des, reportId, score)
-	VALUES (%v, %v, %v);
+	VALUES ('%v', %v, %v);
 	`, req.Des, req.ReportId, req.Score)
 
-	err = stmt.Exec(sql).Error
+	err = stmt.Exec(sql2).Error
 	if err != nil {
 		return err
 	}
 
-	err = stmt.Table("historyReport").Where("reportId = ?", req.ReportId).Update("dateOfIssue = ?", req.DateOfIssue).Error
+	// err = stmt.Table("historyReport").Where("reportId = ?", req.ReportId).Update("dateOfIssue = ?", req.DateOfIssue).Error
+	sql3 := fmt.Sprintf(
+		`
+		UPDATE historyReport 
+		SET dateOfIssue = DATE('%v')
+		WHERE reportId = %v;
+	`, req.DateOfIssue, req.ReportId)
+	err = stmt.Exec(sql3).Error
 	if err != nil {
 		return err
 	}
