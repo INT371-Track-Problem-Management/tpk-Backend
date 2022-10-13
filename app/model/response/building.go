c98dc70d639e5c18799d04bb854f4c60d067cdd4
WinnnieZero <26761@sattha.ac.th>
2022-10-13 15:46:26 +0700

    edit res building

--- app/model/response/building.go
+++ app/model/response/building.go
@@ -1,8 +1,9 @@
 package response
 
 type Building struct {
-	BuildingId int    ` json:"dormId" gorm:"column:DORMID"`
-	Address    string `json:"address" gorm:"column:ADDRESS"`
-	Phone      string `json:"phone" gorm:"column:PHONE"`
-	Email      string `json:"email" gorm:"column:EMAIL"`
+	BuildingId 		int    `json:"buildingId" gorm:"column:BUILDINGID"`
+	BuildingName 	string `json:"buildingName" gorm:"column:BUILDINGNAME"`
+	Address    		string `json:"address" gorm:"column:ADDRESS"`
+	Phone      		string `json:"phone" gorm:"column:PHONE"`
+	Email      		string `json:"email" gorm:"column:EMAIL"`
 }
