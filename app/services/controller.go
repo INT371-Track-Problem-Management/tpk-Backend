00343889b8941b6027491459e120c557665fe64a
artid.vij <artid.vij@innovasive.co.th>
2022-11-04 23:16:28 +0700

    re

--- app/services/controller.go
+++ app/services/controller.go
@@ -6,8 +6,6 @@ import (
 
 type ControllerInterface interface {
 	CheckHealthy(ctx echo.Context) error
-
-	//user
 	Login(ctx echo.Context) error
 	RegisterCustomers(ctx echo.Context) error
 	RegisterOwner(ctx echo.Context) error
--- app/services/service.go
+++ app/services/service.go
@@ -7,7 +7,6 @@ import (
 
 type ServiceInterface interface {
 	CheckHealthy() (*string, error)
-
 	Login(req request.User) (*response.Token, error)
 	RegisterCustomersService(req request.RegisterCustomer) (*int, error)
 	RegisterOwnerService(req request.RegisterOwner) (*int, error)
