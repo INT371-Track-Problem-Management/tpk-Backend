1628f97424a99b34a03c4e9246f7e8af15bec418
artid.vij <artid.vij@innovasive.co.th>
2022-11-20 19:03:05 +0700

    ☀️encode password when chenge pwd

--- app/services/service/change_password.go
+++ app/services/service/change_password.go
@@ -1,15 +1,17 @@
 package service
 
 import (
+	"tpk-backend/app/jwt"
 	"tpk-backend/app/models/model"
 	"tpk-backend/app/models/request"
 )
 
 func (s serviceTPK) ChangePassword(req request.ChangePassword) error {
+	newPwd, _ := jwt.GenerateTokenFromPassword(req.NewPassword)
 	model := model.ChangePassword{
 		Email:       req.Email,
 		OldPassword: req.OldPassword,
-		NewPassword: req.NewPassword,
+		NewPassword: *newPwd,
 	}
 	if err := s.repo.ChangePassword(model); err != nil {
 		return err
